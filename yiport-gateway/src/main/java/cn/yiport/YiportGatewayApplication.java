e9b2ce55b54f9ca40bd3ff62c494f6d6165c78ea
YiPort <yejinbin1024@163.com>
2023-04-11 22:42:09 +0800

    feat: 网关微服务，使用Zuul网关

---
+++ yiport-gateway/src/main/java/cn/yiport/YiportGatewayApplication.java
@@ -0,0 +1,16 @@
+package cn.yiport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class YiportGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiportGatewayApplication.class,args);
+    }
+}
